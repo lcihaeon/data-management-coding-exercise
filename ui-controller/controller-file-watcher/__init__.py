8fae7ac54b48169d929a9f13fbdbefcf9fe20a72
Grace.Liao <grace.liao@sas.com>
2022-11-07 12:11:43 -0500

    hooked up managed services with backend apis

--- ui-controller/controller-file-watcher/__init__.py
+++ ui-controller/controller-file-watcher/__init__.py
@@ -1,24 +1,26 @@
-import logging
+import logging, json
 
 import azure.functions as func
-
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
+    #
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
+    data = requests.get('http://localhost:7071/api/file-watcher')
+    return func.HttpResponse(json.dumps(data.json()), headers={"content-type": "application/json"})
