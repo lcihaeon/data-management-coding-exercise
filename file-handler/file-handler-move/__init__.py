31998aeb82844418759f11c36c5570bf13cee330
Grace.Liao <grace.liao@sas.com>
2022-11-08 09:33:50 -0500

    added mock sftp server

--- file-handler/file-handler-move/__init__.py
+++ file-handler/file-handler-move/__init__.py
@@ -1,24 +1,55 @@
 import logging
 
 import azure.functions as func
+import paramiko
+
+sftp_config = {
+    "host": 'localhost',
+    "port": 3373,
+    "username": 'admin',
+    "password": 'admin',
+    "private_key_file": 'test/test_rsa.key'
+}
+
+
+class SftpServer(object):
+    def __init__(self, config=sftp_config):
+        # pkey = paramiko.RSAKey.from_private_key_file('/tmp/test_rsa.key')
+        # transport = paramiko.Transport(('localhost', 3373))
+        # transport.connect(username='admin', password='admin', pkey=pkey)
+        # sftp = paramiko.SFTPClient.from_transport(transport)
+        pkey = paramiko.RSAKey.from_private_key_file(config['private_key_file'])
+        transport = paramiko.Transport((config['host'], config['port']))
+        transport.connect(username=config['username'], password=config['password'], pkey=pkey)
+        self.sftp = paramiko.SFTPClient.from_transport(transport)
+
+    def start_polling(self, file_pattern):
+        self.sftp.listdir('.')
+        return []
+
+    def get(self, file_remote_path, file_local_path):
+        """ Download files """
+        self.sftp.get(file_remote_path, file_local_path)
+
+    def remove(self, file_remote_path):
+        self.sftp.remove(file_remote_path)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Ready to handle files ...')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    file_list = req.get_json()["files"]
+    xfer_source = req.get_json()["source"]
+    xfer_dest = req.get_json()["destination"]
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    if len(file_list) > 0 and xfer_source == "sftp":
+        sftp = SftpServer()
+
+        for file in file_list:
+            sftp.get(file, file) # TODO: connect with transport storage
+            logging.info(f"{file} has been moved to storage xyz")
+            sftp.remove(file)
+            logging.info(f"{file} has been deleted from sftp")
+
+
+    return func.HttpResponse(status_code=200)
--- file-watcher/file-watcher/__init__.py
+++ file-watcher/file-watcher/__init__.py
@@ -1,20 +1,65 @@
-import json
+import json, requests
 import logging
 
 import azure.functions as func
+import paramiko
+import fnmatch
+import pdb
+
+sftp_config = {
+    "host": 'localhost',
+    "port": 3373,
+    "username": 'admin',
+    "password": 'admin',
+    "private_key_file": 'test/test_rsa.key'
+}
+FILE_HANDLER_URL = 'http://localhost:7073/api/file-handler'
+
+
+class SftpServer(object):
+    def __init__(self, config=sftp_config):
+        # pkey = paramiko.RSAKey.from_private_key_file('/tmp/test_rsa.key')
+        # transport = paramiko.Transport(('localhost', 3373))
+        # transport.connect(username='admin', password='admin', pkey=pkey)
+        # sftp = paramiko.SFTPClient.from_transport(transport)
+        # pdb.set_trace()
+        pkey = paramiko.RSAKey.from_private_key_file(config['private_key_file'])
+        transport = paramiko.Transport((config['host'], config['port']))
+        transport.connect(username=config['username'], password=config['password'], pkey=pkey)
+        self._sftp = paramiko.SFTPClient.from_transport(transport)
+
+    def start_polling(self, file_pattern):
+        matching_files = []
+        for f in self._sftp.listdir('.'):
+            if fnmatch.fnmatch(f, file_pattern):
+                matching_files.append(f)
+
+        return matching_files
+
+    def get(self, file_remote_path, file_local_path):
+        """ Download files """
+        self._sftp.get(file_remote_path, file_local_path)
+
+    def remove(self, file_remote_path):
+        self._sftp.remove(file_remote_path)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info(f"Request with method {req.method} received.")
-    if req.method == "POST":
-        action = req.get_json()["action"]
-        if action == "start":
-            return func.HttpResponse(json.dumps({"status": "running", "message": None, "error": None}))
-        elif action == "stop":
-            return func.HttpResponse(json.dumps({"status": "stopped", "message": None, "error": None}))
-        else:
-            # defaults to check status
-            return func.HttpResponse(json.dumps({"status": "running", "message": None, "error": None}))
+    logging.info("File Watcher Ready ...")
+
+    sftp = SftpServer()
+    trigger_files = sftp.start_polling("*.trg")
+
+    logging.info(f"trigger_files: {trigger_files}")
+
+    if len(trigger_files) > 0:
+        logging.info(f"Trigger files have been detected. Number of files: {len(trigger_files)}.")
+        logging.info(f"Start to transfer files: {trigger_files}")
+
+        data = {"files": trigger_files, "source": "sftp", "destination": "transfer"}
+        requests.post(FILE_HANDLER_URL, data=data)
+        # logging.info(f"Making HTTP request for data: {data}")
     else:
-        # defaults to check status
-        return func.HttpResponse(json.dumps({"status": "running", "message": None, "error": None}))
+        logging.info("No trigger files found.")
+
+    return func.HttpResponse(status_code=200)
